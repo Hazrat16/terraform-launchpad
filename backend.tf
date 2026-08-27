80fd2d48e41428a2f6170002dc023fb97d5dcb93
hazrat <hazrat17016@gmail.com>
2026-08-27 10:44:21 +0600

    Update S3 bucket name in backend configuration for Terraform

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "s3" {
-    bucket       = "nure-64573-bucket-123"
+    bucket       = "haz-2312-bucket-123"
     key          = "terraform-launchpad/dev/terraform.tfstate"
     region       = "us-west-2"
     encrypt      = true
